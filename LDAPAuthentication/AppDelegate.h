390957b2609634ac41c2e06665ca68eec1ec0f39
Firdous <firdous.ali86@gmail.com>
2016-02-24 10:42:01 +0500

    Initial Commit

---
+++ LDAPAuthentication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDAPAuthentication
+//
+//  Created by Firdous on 24/02/2016.
+//  Copyright © 2016 TenPearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDAPAuthentication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDAPAuthentication
+//
+//  Created by Firdous on 24/02/2016.
+//  Copyright © 2016 TenPearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
